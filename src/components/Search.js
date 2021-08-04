27383a74d8e4bd07f3f61337133f879b6bbff48c
RajatUba-LKS <rajat.member24@lakshmisri.com>
2021-08-04 15:28:57 +0530

    Added date range in Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
+import {DateRangePicker} from 'react-date-range';
+import '../css/Navbar.css';
+//date picker
+class Search extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      showDatePicker:false,
+      selectionRange : {
+        startDate:new Date(),
+        endDate: new Date(),
+        key:'date_range'
+      }
+    }
+  }
+  showDateRange=()=>{
+   this.setState((prevState)=>{
+     return {
+       showDatePicker:!prevState.showDatePicker
+     }
+   })   
+  }
+  handleSelect=(ranges)=>{
+    console.log(ranges);
+  }
 
-function Search(){
+  render(){
+    
+    const {showDatePicker,selectionRange}=this.state;
     return (
-        <div className="searchBar">Search</div>
+      <div className="searchBar">
+          <div className="search" onClick={this.showDateRange}>Search</div>
+          
+          { showDatePicker &&
+            <DateRangePicker
+              ranges={[selectionRange]}
+              onChange={this.handleSelect}
+            />
+          }
+
+          <button >S</button>
+      </div>
     );
+  }
 }
 
 export default Search;
